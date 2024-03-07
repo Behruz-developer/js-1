7ae2e144dc9768e3ee503ae5ba13f63cc08b34d7
Behruz-developer <88540036+Behruz-developer@users.noreply.github.com>
2024-03-07 16:21:35 +0500

    js-1

---
+++ script.js
@@ -0,0 +1,25 @@
+var name1 = prompt('Ismingizni kriting:')
+console.log("Mening ismim " + name1);
+
+var age = prompt('Yoshingizni kriting:')
+console.log("Men " + age + " yoshdaman");
+
+var misol1 = prompt('Misolni yeching: 10 + 15 = ?')
+console.log("Misol 1: 10 + 15 = 25 Sizning javob " + misol1);
+
+var misol2 = prompt('Misolni yeching: 55 - 14 = ?')
+console.log("Misol 2: 55 - 14 = 41 Sizning javob " + misol2);
+
+var misol3 = prompt('Misolni yeching: 7 * 6 = ?')
+console.log("Misol 3: 7 * 6 = 42 Sizning javob " + misol3);
+
+var misol4 = prompt('Misolni yeching: 48 / 6 = ?')
+console.log("Misol 4: 48 / 6 = 8 Sizning javob " + misol4);
+
+var misol5 = prompt('Misolni yeching: 55 % 2 = ?')
+console.log("Misol 5: 55 % 2 = 1 Sizning javob " + misol5);
+
+alert('Natijani ko\'ish uchun konsolni oching')
+
+
+
